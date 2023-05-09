bd29a07efde08d12d39c935b2fce16697968ad3d
Rifaath <rifaath.mohamedrifki@tamara.co>
2023-05-09 10:24:41 +0400

    Building the coach detail component

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,6 +1,65 @@
 <template>
-  <div>Details for coach</div>
+  <!-- <div>Details for coach</div>
   <router-view></router-view>
-  <!-- router-view is used to render components that are part of the children array configured in the routes, without it the child component will not be displayed, router-link alone will work for components that are not part of the children array -->
-  <router-link to="/coaches/c1/contact">Contact</router-link>
+ router-view is used to render components that are part of the children array configured in the routes, without it the child component will not be displayed, router-link alone will work for components that are not part of the children array -->
+  <!-- <router-link to="/coaches/c1/contact">Contact</router-link> -->
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink">Contact</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      />
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.firstName + '' + this.selectedCoach.lastName;
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+    contactLink() {
+      return this.$route.path + '/' + this.id + '/contact';
+    },
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      (coach) => coach.id === this.id
+    );
+  },
+};
+</script>
