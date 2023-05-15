034c305f01bcb8304b24d8adbd6cb22cba34bd37
Rifaath <rifaath.mohamedrifki@tamara.co>
2023-05-15 19:28:17 +0400

    Sending a PUT Http Request to store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,27 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     }
+    // When connecting to firebase, make sure to add .json to the end of the link
+    const response = await fetch(`https://vue-http-demo-9b04d-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+      method: 'PUT', // Put means that it will overide the existing data or create new data if the data does not exist
+      body: JSON.stringify(coachData)
+    })
 
-    context.commit('registerCoach', coachData)
+    // const responseData = await response.json()
+
+    if (!response.ok) {
+      // error ...
+    }
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    })
   }
 }
