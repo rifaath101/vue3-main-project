d6af0b5cc59e0bdb4e863db200f159016d5b9142
Rifaath <rifaath.mohamedrifki@tamara.co>
2023-05-17 21:23:35 +0400

    Adding Http Error Handling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,8 @@
 <template>
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+    <!--!!error means to convert error to a boolean value if it's not truthy-->
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <coach-filter @change-filter="setFilters" />
   </section>
@@ -38,6 +42,7 @@ export default {
   data() {
     return {
       isLoading: false,
+      error: null,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -77,8 +82,15 @@ export default {
     },
     async loadCoaches() {
       this.isLoading = true;
-      await this.$store.dispatch('coaches/loadCoaches');
+      try {
+        await this.$store.dispatch('coaches/loadCoaches');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
       this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
