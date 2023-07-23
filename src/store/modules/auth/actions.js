5d0e6d0684a5d56e2af8a44617b04abe35c81d80
Rifaath <rifaath.mohamedrifki@tamara.co>
2023-07-23 17:52:43 +0400

    Adding a signup action and flow

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,34 @@
-export default {};
+export default {
+  login() {},
+  async signup(context, payload) {
+    console.log(payload.email, ' payload.email');
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDr4LWc8U2dcN1dyhV_gF7eK9yRQ84-wj8',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
